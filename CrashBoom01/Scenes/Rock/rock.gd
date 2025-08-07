f4f7661e79fa36b97320a0dbbc5044f48e48dcf0
Greg Gallardo <greggallardo@gmail.com>
2025-08-06 19:58:37 -0500

    adding Rock scene for the crash

---
+++ CrashBoom01/Scenes/Rock/rock.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed = 200
+
+var bottom = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	bottom = get_viewport_rect().end.y + $Sprite2D.texture.get_size().y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y += speed * delta
+	if position.y > bottom:
+		queue_free()
