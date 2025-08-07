19f69620ff328a41044fefefb6144cf598fa62cd
Greg Gallardo <greggallardo@gmail.com>
2025-08-07 01:04:35 -0500

    working on asteroids

--- GodotRocks/Scenes/Main/main.gd
+++ GodotRocks/Scenes/Main/main.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@export var rock_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in 10:
+		create_rock()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+
+	if Input.is_action_just_pressed("options"):
+		print("QUIT")
+		get_tree().quit()
+
+
+func create_rock() -> void:
+	var vel = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(5,10)
+	var pos = Vector2( randi_range( 0, 1280), -128 )
+	var rock = rock_scene.instantiate()
+	rock.start(pos, vel)
+	call_deferred("add_child", rock)
--- GodotRocks/Scenes/Rock/rock.gd
+++ GodotRocks/Scenes/Rock/rock.gd
@@ -1,11 +1,33 @@
 extends Area2D
+ 
+var radius = 128
+var velocity: Vector2= Vector2.ZERO
 
+var hit_points = 30
 
+func start( pos, vel ) -> void:
+	position = pos  
+	velocity = vel
+	hit_points = 30
+	
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready() -> void: 
+	radius = $CollisionShape2D.shape.radius
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	position += velocity #* delta
+	
+	# TODO:  FIX THESE HARD CODED VALUES
+	if position.x < 0:
+		position.x = 2560
+	elif position.x > 2560:
+		position.x = 0
+
+	if position.y < 0:
+		position.y =1600
+	elif position.y > 1600:
+		position.y = 0
+ 
