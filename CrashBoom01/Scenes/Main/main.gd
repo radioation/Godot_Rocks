7040d7fde99aed9028f647654658d7cbc32b8c55
Greg Gallardo <greggallardo@gmail.com>
2025-08-06 20:04:22 -0500

    adding rocks to main scene with timer

--- CrashBoom01/Scenes/Main/main.gd
+++ CrashBoom01/Scenes/Main/main.gd
@@ -1,12 +1,27 @@
 extends Node2D
 
+@export var rock_scene : PackedScene
+
 var screensize = Vector2.ZERO
+var rock_start_y = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screensize = get_viewport().get_visible_rect().size
 
+	$RockSpawnTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func create_rock():
+	var rock = rock_scene.instantiate()
+	add_child(rock)
+	rock.position = Vector2( randi_range(0, screensize.x), rock_start_y)
+	
+
+
+func _on_rock_spawn_timer_timeout() -> void:
+	create_rock()
