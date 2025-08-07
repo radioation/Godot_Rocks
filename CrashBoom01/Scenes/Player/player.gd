36012d98093761e30fbf3faadf374644a5dbad30
Greg Gallardo <greggallardo@gmail.com>
2025-08-06 20:08:18 -0500

    Detect 'CRASH'

--- CrashBoom01/Scenes/Player/player.gd
+++ CrashBoom01/Scenes/Player/player.gd
@@ -13,5 +13,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	print( velocity )
+	#print( velocity )
 	position += speed * velocity * delta
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("rocks"): 
+		print("CRASH!!")
+		$CollisionShape2D.set_deferred("disabled", true )
+		hide() 
--- CrashBoom01/Scenes/Rock/rock.gd
+++ CrashBoom01/Scenes/Rock/rock.gd
@@ -15,3 +15,7 @@ func _process(delta: float) -> void:
 	position.y += speed * delta
 	if position.y > bottom:
 		queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
