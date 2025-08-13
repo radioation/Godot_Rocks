0237ec180565c0f7690537d17af3fb95e7e19da9
Greg Gallardo <greggallardo@gmail.com>
2025-08-12 21:31:39 -0500

    adding basic motion to minimap test

--- MiniMap/Scenes/Main/main.gd
+++ MiniMap/Scenes/Main/main.gd
@@ -1,1 +1,22 @@
 extends Node2D
+
+@export var rock_scene : PackedScene
+
+var playarea : Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	playarea = get_viewport().get_visible_rect().size
+	# make rocks
+	spawn_rocks(20)
+	$Ship.playarea = playarea
+	$Ship.position = playarea/2.0
+ 
+	
+func spawn_rocks( count : int ) -> void:
+	for i in count:
+		var r = rock_scene.instantiate()	
+		r.playarea = playarea
+		r.position = Vector2( randf_range(0, playarea.x), randf_range(0, playarea.y))
+		r.linear_velocity = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range( 225.0, 250.0)
+		add_child(r)
+ 
