59d7a0de113be6ce865e17fcb689539cf09a14bb
radioation <greggallardo@gmail.com>
2026-07-18 20:49:35 -0500

    experimenting with mobile controls

--- GodotRocks/Scenes/HUD/hud.gd
+++ GodotRocks/Scenes/HUD/hud.gd
@@ -3,7 +3,10 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if not OS.has_feature("mobile"):
+		$VirtualJoystick.hide()
+		$VirtualJoystick2.hide()
+		$Control/TouchScreenButton.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +24,15 @@ func set_lives(new_lives: int) -> void:
 func set_message( msg: String ) -> void:
 	$VBoxContainer/MessageLabel.text = msg
 	
+
+
+func _on_button_pressed() -> void:
+	var ev_down = InputEventAction.new()
+	ev_down.action = "start"
+	ev_down.pressed = true
+	Input.parse_input_event(ev_down)
+	
+	#var ev_up = InputEventAction.new()
+	#ev_up.action = "start"
+	#ev_up.pressed = false
+	#Input.parse_input_event(ev_up)
